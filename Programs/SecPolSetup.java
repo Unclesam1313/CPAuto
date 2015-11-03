fa7521f9871a82fed1c3b4d07dc821fe80ee81ef
Unclesam1313 <sam.luttrull@gmail.com>
2015-11-02 21:20:30 -0600

    Update SecPolSetup.java

--- Programs/SecPolSetup.java
+++ Programs/SecPolSetup.java
@@ -4,11 +4,19 @@ public class SecPolSetup
 	private Process p;
 	
 	public SecPolSetup()
-	{
+	{	
+		try
+		{
 		ProcessBuilder builder = new ProcessBuilder();
 		builder.command("cmd.exe", "/c", "Secedit /import /db \"C:\\%Windows%\\security\\database\\secedit.sdb\" /cfg \"C:\\CPAuto\\StandPol.inf\"");
 		builder.redirectErrorStream(true);
 		p = builder.start();
+		}
+		catch(Exception e)
+		{
+			System.out.println("An error occured:");
+			e.printStackTrace();
+		}
 	}
 
 	public static void main(String[] args) throws Exception 
