e7b95ee26dca25308b5fd05f2f3b39b7a3f179d1
Unclesam1313 <sam.luttrull@gmail.com>
2015-11-14 17:41:13 -0600

    Create MFile.java

---
+++ Programs/MFile.java
@@ -0,0 +1,36 @@
+package media;
+
+public class MFile 
+{
+	private String path;
+	
+	public boolean isFile()
+	{
+		return true;
+	}
+	
+	public boolean isFolder()
+	{
+		return false;
+	}
+	
+	public void delete() 
+	{
+		try
+		{
+			ProcessBuilder b = new ProcessBuilder("cmd.exe", "/c", "del " + path);
+			Process p = b.start();
+			p.destroy();
+		}
+		catch(Exception e)
+		{
+			System.out.println("An Error Occured!:");
+			e.printStackTrace();
+		}
+	}
+	
+	public String path()
+	{
+		return path;
+	}
+}
