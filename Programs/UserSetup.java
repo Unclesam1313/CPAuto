353d65c3d942c6db48777884086658e740b01a6b
Unclesam1313 <sam.luttrull@gmail.com>
2015-11-14 14:44:42 -0600

    Update UserSetup.java

--- Programs/UserSetup.java
+++ Programs/UserSetup.java
@@ -258,44 +258,4 @@ public class UserSetup
 		
 		return result;
 	}
-	
-	/*public static void main(String[] args) throws IOException
-	{
-		if(!adminTest())
-		{
-			System.out.println("Please Run as an Admin");
-			System.exit(0);
-		}
-		final JFileChooser fc = new JFileChooser();
-		fc.showDialog(null, "Use File");
-		new UserSetup(fc.getSelectedFile());
-		//new UserSetup(null);
-	}*/
-	
-	/*public static boolean adminTest()  
-	{
-		File dir = new File("C:/Program Files");
-		
-	    if (dir.canWrite() == false)
-	        return false;
-
-	    if (!dir.isDirectory())
-	        return false;
-
-	    File fileTest = null;
-	    try 
-	    {
-	        fileTest = File.createTempFile("writeableArea", ".dll", dir);
-	    } 
-	    catch (IOException e) 
-	    {
-	        return false;
-	    } 
-	    finally 
-	    {
-	        if (fileTest != null)
-	            fileTest.delete();
-	    }
-	    return true;
-	}*/
 }
