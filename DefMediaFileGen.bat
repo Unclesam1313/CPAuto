f093085810de876cbed13bff6d36343d2e4c2429
Unclesam1313 <sam.luttrull@gmail.com>
2015-11-14 20:49:50 -0600

    Create DefMediaFileGen.bat

---
+++ DefMediaFileGen.bat
@@ -0,0 +1,3 @@
+cd C:\
+dir /b /s *.bmp *.asf *.wma *.wmv *.wm *.asx *.wax *.wvx *.wmx *.wpl *.dvr-ms *.wmd *.avi *.mpg *.mpeg *.m1v *.mp2 *.mp3 *.mpa *.mpe *.m3u *.mid *.midi *.rmi *.aif *.aifc *.aif *.au *.snd *.wav *.cda *.ivf *.wmz *.wms *.mov *.m4a *.mp4 *.m4v *.mp4v *.3g2 *.3gp2 *.3gp *.3gpp *.aac *.adt *.adts *.m2ts *.tif *.tiff *.gif *.jpeg *.jpg *.jif *.jfif *.jp2 *.jpx *.j2k *.j2c *.fpx *.pcd *.png *.pdf > %~dp0\files.txt
+pause
