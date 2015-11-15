8c5ac31a92b4277b5b8564c2f00e11b8300913f9
Unclesam1313 <sam.luttrull@gmail.com>
2015-11-14 23:40:02 -0600

    Update MTester.java

--- Programs/MTester.java
+++ Programs/MTester.java
@@ -18,7 +18,9 @@ public class MTester
 		{
 			for(int i = level; i > 0; i--)
 				System.out.print("\t");
-			System.out.println(item.path().substring(item.path().lastIndexOf('\\')));
+			
+			String[] splitPath = item.path.split("[\\\\]");
+			System.out.println(splitPath[splitPath.length-2);
 			for(MItem itm: ((MFolder)item).contents())
 				print(itm, level+1);
 		}
