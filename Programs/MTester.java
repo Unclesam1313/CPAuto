d5940d24fc0c4b1bb6514a2ec0df0381e4616233
Unclesam1313 <sam.luttrull@gmail.com>
2015-11-14 23:15:53 -0600

    Create MTester.java

---
+++ Programs/MTester.java
@@ -0,0 +1,28 @@
+package media;
+
+public class MTester 
+{
+	public static void main(String[] args)
+	{
+		MFolder root = MBuild.genList();
+		print(root,0);
+	}
+	
+	private static void print(MItem item, int level)
+	{
+		if(item.isFile())
+		{
+			for(int i = level; i > 0; i--)
+				System.out.println("\t");
+			System.out.println(item.path());
+		}
+		else if(item.isFolder())
+		{
+			for(int i = level; i > 0; i--)
+				System.out.println("\t");
+			System.out.println(item.path());
+			for(MItem itm: ((MFolder)item).contents())
+				print(itm, level+1);
+		}
+	}
+}
