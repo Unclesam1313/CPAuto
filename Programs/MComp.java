7071e33c2f3d82aa17349be453b389a04ab7b9c6
Sam Luttrull <Unclesam1313@users.noreply.github.com>
2016-01-24 02:46:31 -0600

    Update MComp.java

--- Programs/MComp.java
+++ Programs/MComp.java
@@ -1,15 +1,15 @@
 import java.io.*;
 import java.util.*;
 
-public class MComp 
+public class MComp
 {
 	private final String EXTENSIONS = "*.avi *.mpg *.vob *.mp4 *.m2ts *.mov *.3gp *.mkv *.m4a *.m4b *.mp3 *.wav *.wma *.webm *.wv *.flac";
-	
+
 	public MComp() throws IOException
 	{
 
-		ProcessBuilder b = new ProcessBuilder("cmd.exe", "/c","cd C:\\ && dir /s /b " + EXTENSIONS);
-		b.redirectErrorStream(true);			
+		ProcessBuilder b = new ProcessBuilder("cmd.exe", "/c","cd /d C:\\ && dir /s /b " + EXTENSIONS);
+		b.redirectErrorStream(true);
 		Process p = b.start();
 		BufferedReader r = new BufferedReader(new InputStreamReader(p.getInputStream()));
 		String line;
@@ -21,29 +21,30 @@ public class MComp
 			lines.add(line);
 		}
 		r.close();
-		
+
 		process(lines);
 	}
-	
+
 	private void process(List<String> lines)
 	{
-		/*for(String path: lines)
-			System.out.println(path);*/
+		MItem base = MBuild.genList();
+		for(String path: lines)
+			System.out.println(path);
 
 		for(int i = lines.size() - 1; i >= 0; i--)
 		{
 			String[] path = lines.get(i).split("[\\\\]");
 			if(path.length > 1 && (path[1].equals("Program Files") || path[1].equals("Program Files (x86)")|| path[1].equals("Program Data")))
 				lines.remove(i);
-			else if(search(MBuild.genList(), path, 0))
+			else if(search(base, path, 0))
 				lines.remove(i);
 		}
-		
+
 		System.out.println("\nItems to delete:\n");
 		for(String path: lines)
 			System.out.println(path);
 	}
-	
+
 	private boolean search(MItem dir, String[] path, int i)
 	{
 		String[] dirPath = dir.path().split("[\\\\]");
@@ -68,7 +69,7 @@ public class MComp
 		else
 			return false;
 	}
-	
+
 	public static void main(String[] args)
 	{
 		try
