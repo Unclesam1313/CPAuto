f3a94a7d71bda1070d07ac0b2ccf94d6bb0e393a
Sam Luttrull <Unclesam1313@users.noreply.github.com>
2016-01-24 01:11:08 -0600

    Update DefMediaFileGen.bat

--- DefMediaFileGen.bat
+++ DefMediaFileGen.bat
@@ -1,3 +1,3 @@
 cd C:\
-dir /b /s *.bmp *.asf *.wma *.wmv *.wm *.asx *.wax *.wvx *.wmx *.wpl *.dvr-ms *.wmd *.avi *.mpg *.mpeg *.m1v *.mp2 *.mp3 *.mpa *.mpe *.m3u *.mid *.midi *.rmi *.aif *.aifc *.aif *.au *.snd *.wav *.cda *.ivf *.wmz *.wms *.mov *.m4a *.mp4 *.m4v *.mp4v *.3g2 *.3gp2 *.3gp *.3gpp *.aac *.adt *.adts *.m2ts *.tif *.tiff > %~dp0\files.txt
+dir /b /s *.avi *.mpg *.vob *.mp4 *.m2ts *.mov *.3gp *.mkv *.m4a *.m4b *.mp3 *.wav *.wma *.webm *.wv *.flac > %~dp0\files.txt
 pause
